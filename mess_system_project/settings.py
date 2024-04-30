c705f3136db589a3a3d50b69fc5a9c2772301e29
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-04-30 20:20:59 +0530

    Bugs resolved

--- mess_system_project/settings.py
+++ mess_system_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--ym)lkmsm6@jv1p_%2+ri4b$eh-emah2q@jhd5fb260lna758_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app",".now.sh","127.0.0.1"]
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mess_system_project.wsgi.application'
+WSGI_APPLICATION = 'mess_system_project.wsgi.app'
 
 
 # Database
