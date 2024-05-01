7e9d728e118b55a5bfa70b8c1a1a69868f1e3eda
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-05-01 21:36:16 +0530

    Added requirement files7

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,6 @@
 # build_files.sh
-python3.9 -m venv myenv
-source myenv/Scripts/activate
 pip3 install -r requirements.txt
 
 # make migrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic 
-python3.9 manage.py runserver
